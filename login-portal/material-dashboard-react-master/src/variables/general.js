4863f667296194ae5686739f304e49d841a76f74
Shruti Umat <official.wildcat26@gmail.com>
2021-02-11 16:24:48 +0530

    Customizing React template

---
+++ login-portal/material-dashboard-react-master/src/variables/general.js
@@ -0,0 +1,26 @@
+// ##############################
+// // // Tasks for TasksCard - see Dashboard view
+// #############################
+
+var tasks = [
+  "Sign contract with Beverley client",
+  "Assessing losses and gains from the last deal",
+  "Create 4 Invisible user experiences"
+];
+var followUp = [
+  "Discuss design changes",
+  "Code review by Josh",
+  "Review benchmarks for API response times"
+];
+var meetings = [
+  "John - monthly performance review - 10 AM today",
+  "Indiana - talk about design changes - 11 AM tomorrow",
+  "Tom - prepare for the cultural night - 1 PM day after tomorrow"
+];
+
+module.exports = {
+  // these 3 are used to create the tasks lists in TasksCard - Dashboard view
+  tasks,
+  followUp,
+  meetings 
+};

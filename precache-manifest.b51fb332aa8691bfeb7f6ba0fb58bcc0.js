79d1450aaaa8c503086299232b77abd16b9677c9
Shruti Umat <official.wildcat26@gmail.com>
2021-02-12 17:48:24 +0530

    Updates

---
+++ precache-manifest.b51fb332aa8691bfeb7f6ba0fb58bcc0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b37fda3426cbf81b15306a529e00922",
+    "url": "/Personal-Assistant/index.html"
+  },
+  {
+    "revision": "467cda38ba42ba170fff",
+    "url": "/Personal-Assistant/static/css/2.5f981152.chunk.css"
+  },
+  {
+    "revision": "df149af7c5f1b04aa829",
+    "url": "/Personal-Assistant/static/css/main.8e9f8467.chunk.css"
+  },
+  {
+    "revision": "467cda38ba42ba170fff",
+    "url": "/Personal-Assistant/static/js/2.377df1b1.chunk.js"
+  },
+  {
+    "revision": "34491c6de1fbcf2388878b87785c74e7",
+    "url": "/Personal-Assistant/static/js/2.377df1b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df149af7c5f1b04aa829",
+    "url": "/Personal-Assistant/static/js/main.dd282158.chunk.js"
+  },
+  {
+    "revision": "f7bdf0cfc9ea0dd44285692948c2d84f",
+    "url": "/Personal-Assistant/static/js/main.dd282158.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cf12879525002fcf0ec",
+    "url": "/Personal-Assistant/static/js/runtime-main.274f802a.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/Personal-Assistant/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/Personal-Assistant/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/Personal-Assistant/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/Personal-Assistant/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/Personal-Assistant/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);

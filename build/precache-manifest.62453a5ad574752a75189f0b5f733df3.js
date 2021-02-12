f66ae11b92b23321b5225096036b5c2fad5b7204
Shruti Umat <official.wildcat26@gmail.com>
2021-02-12 17:01:22 +0530

    Fix directory structure

---
+++ build/precache-manifest.62453a5ad574752a75189f0b5f733df3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55e32921df59bb40e1eadd82265641ed",
+    "url": "/material-dashboard-react/index.html"
+  },
+  {
+    "revision": "467cda38ba42ba170fff",
+    "url": "/material-dashboard-react/static/css/2.5f981152.chunk.css"
+  },
+  {
+    "revision": "df149af7c5f1b04aa829",
+    "url": "/material-dashboard-react/static/css/main.8e9f8467.chunk.css"
+  },
+  {
+    "revision": "467cda38ba42ba170fff",
+    "url": "/material-dashboard-react/static/js/2.377df1b1.chunk.js"
+  },
+  {
+    "revision": "34491c6de1fbcf2388878b87785c74e7",
+    "url": "/material-dashboard-react/static/js/2.377df1b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df149af7c5f1b04aa829",
+    "url": "/material-dashboard-react/static/js/main.dd282158.chunk.js"
+  },
+  {
+    "revision": "f7bdf0cfc9ea0dd44285692948c2d84f",
+    "url": "/material-dashboard-react/static/js/main.dd282158.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a025ab54ec03fed337bb",
+    "url": "/material-dashboard-react/static/js/runtime-main.d0bf2bb1.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/material-dashboard-react/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/material-dashboard-react/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/material-dashboard-react/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/material-dashboard-react/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/material-dashboard-react/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
